9250ea6f9e149a304ad258a4cbdb0e3e1c46f31f
Sydney Dunlap <sydneydunlap@Sydneys-MacBook-Pro.local>
2018-12-11 20:00:13 -0500

    nothing works

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,40 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyAu6PqMN6N4QlsttiRI9U6PT3SVaGXuy6w",
-    authDomain: "musicweather-fe8c7.firebaseapp.com",
-    databaseURL: "https://musicweather-fe8c7.firebaseio.com",
-    projectId: "musicweather-fe8c7",
-    storageBucket: "musicweather-fe8c7.appspot.com",
-    messagingSenderId: "589121883549"
-  };
-  firebase.initializeApp(config);
+$(document).ready(function () {
 
-// Weather App API Key
-var APIKey = "13783c874e54ca4e2de546d0430362f0";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-userCity + APIKey;
+  // Initialize Firebase
+  // var config = {
+  //     apiKey: "AIzaSyAu6PqMN6N4QlsttiRI9U6PT3SVaGXuy6w",
+  //     authDomain: "musicweather-fe8c7.firebaseapp.com",
+  //     databaseURL: "https://musicweather-fe8c7.firebaseio.com",
+  //     projectId: "musicweather-fe8c7",
+  //     storageBucket: "musicweather-fe8c7.appspot.com",
+  //     messagingSenderId: "589121883549"
+  //   };
+  //   firebase.initializeApp(config);
 
-// Get Weather
-$.ajax({
-    url: queryURL,
-    method: "GET"
+  // Weather App API Key
+  var APIKey = "13783c874e54ca4e2de546d0430362f0";
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + APIKey;
+
+  // Click event for submit button
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
+    var name = $("#name").val().trim();
+    var location = $("#location").val().trim();
+    console.log(name);
+    console.log(location);
+
+    // Get Weather
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+      .then(function (response) {
+        console.log(queryURL);
+        console.log(response);
+      })
   })
+
+
+
+})
